d8f339c926d8b8d21cea40b37b361e0f603056a7
lexy <lexyblazy@gmail.com>
2018-06-07 03:18:41 +0100

    completed client side views

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+const iconsContainer = {
+  fontSize: 60,
+  textAlign: "center"
+};
 class App extends Component {
+  icons = ["map-marker-alt", "taxi", "glass-martini"];
+  renderIcons = () => {
+    return this.icons.map(icon => {
+      return (
+        <i key={icon} style={{ margin: 10 }} className={`fa fa-${icon}`} />
+      );
+    });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container" style={{ textAlign: "center" }}>
+        <h1>PLANS TONIGHT ?</h1>
+        <div style={iconsContainer}>{this.renderIcons()}</div>
+        <p>See which bars are hoppin' tonight and RSVP ahead of time!</p>
+        <p>Remember: take a cab and drink responsibly.</p>
+        <div>
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="WHERE YOU AT ?"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+              style={{backgroundColor: 'inital'}}
+            />
+            <div class="input-group-append">
+              <span class="input-group-text btn">GO</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
